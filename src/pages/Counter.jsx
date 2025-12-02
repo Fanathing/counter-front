9bc8a3db431d3cc3005dc417a6c5971235abc5e1
Fanathing <mmybonnie@gamil.com>
2025-12-02 11:45:18 +0900

    Feat: 카운터 백 api를 이용하여 증가

--- src/pages/Counter.jsx
+++ src/pages/Counter.jsx
@@ -1,17 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const Main = () => {
+const Counter = () => {
 
     const [count, setCount] = useState(1);
     
+    useEffect(() => {
+        const getCount = async() => {
+            const { data } = await axios.get(`${process.env.REACT_APP_BACK_API}/`);
+            setCount(data.count);
+        }
+        getCount();
+    }, [])
+
+    const fetchIncrement = async() => {
+        const { data } = await axios.post(`${process.env.REACT_APP_BACK_API}/increment`);
+        setCount(data.count);
+    }
+
     return (
         <>
             <div>이곳은 카운터 페이지 입니다!</div>
             {count}
-            <button onClick={() => setCount(count + 1)}>+</button>
+            <button onClick={fetchIncrement}>+</button>
             <button onClick={() => setCount(count - 1)}>-</button>
         </>
     )
 }
 
-export default Main;
+export default Counter;
